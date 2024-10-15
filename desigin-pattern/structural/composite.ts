16abd6ef1fda985ade34fb7a277c46a3411f57ad
Vigneshrajan <Ankithvicky@gmail.com>
2024-10-15 17:25:02 +0530

    Update composite.ts

--- desigin-pattern/structural/composite.ts
+++ desigin-pattern/structural/composite.ts
@@ -39,3 +39,48 @@ mainTaskList.addTask(task1);
 mainTaskList.addTask(task2);
 mainTaskList.addTask(taskList3);
 mainTaskList.display();
+
+
+
+interface IProduct {
+    getCost(): number;
+}
+
+
+class Product implements IProduct {
+    name: string;
+    cost: number;
+    constructor(name: string, cost: number) {
+        this.name = name;
+        this.cost = cost;
+    }
+    getCost() {
+        return this.cost
+    }
+}
+class Box implements IProduct {
+    products: IProduct[] = []
+    cost: number;
+    constructor(cost: number) {
+        this.cost = cost;
+    }
+    addProduct(product: IProduct) {
+        this.products.push(product);
+    }
+    getCost(): number {
+        return this.cost + this.products.reduce((a, c) => a + c.getCost(), 0)
+    }
+}
+
+
+const mainBox = new Box(10);
+const subBox1 = new Box(2);
+subBox1.addProduct(new Product("Toy1", 100))
+subBox1.addProduct(new Product("Toy2", 200))
+const subBox2 = new Box(2);
+subBox2.addProduct(new Product("Toy3", 10))
+subBox2.addProduct(new Product("Toy4", 20))
+mainBox.addProduct(subBox1);
+mainBox.addProduct(subBox2);
+
+console.log(mainBox.getCost());
