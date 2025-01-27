6a85d32e38d1590485debee9c1743cd4b5d32085
Vigneshrajan <Ankithvicky@gmail.com>
2025-01-27 20:31:30 +0530

    Update singleton.ts

--- desigin-pattern/creational/singleton.ts
+++ desigin-pattern/creational/singleton.ts
@@ -10,6 +10,14 @@ class MongoDb {
 }
 
 /**
+
+Procedure
+1. Mark the class constructor as private.
+2. Create a static method to get the instance of the class.
+3. Access the constructor and create the object.
+4. Store the created object in a static field.
+5. Return the stored values.
+
  * 1. Useful when accessing shared resources. Saying the code base should have a single instance and 
  * all the code will get that instance to play around.
  * 2. It can be used as a global variable by overriding its value should be restricted.
